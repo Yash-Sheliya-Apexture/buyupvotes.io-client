acddb1c8313f248ca9486916942b03652ded53a5
Yash-Sheliya-Apexture <yash@apexture.in>
2024-12-23 11:39:25 +0530

    new code add

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import WebsiteRoutes from "./routes/WebsiteRoutes";
 import DashboardRoutes from "./routes/DashboardRoutes";
 import ProtectedRoute from "./routes/ProtectedRoute"; // Import ProtectedRoute
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'; // Don't forget to import the CSS for react-toastify
 import { AuthProvider } from "./auth/AuthContext";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Router>
+    <Router>
+      <AuthProvider>
         {/* ToastContainer added globally */}
         <ToastContainer
           autoClose={3000}
@@ -27,13 +28,13 @@ const App = () => {
           <Route
             path="/dashboard/*"
             element={
-              // <ProtectedRoute element={<DashboardRoutes />} /> // Protecting dashboard
-              <DashboardRoutes />
+              <ProtectedRoute element={<DashboardRoutes />} /> // Protecting dashboard
+              // <DashboardRoutes />
             }
           />
         </Routes>
-      </Router>
-    </AuthProvider>
+      </AuthProvider>
+    </Router>
   );
 };
 
