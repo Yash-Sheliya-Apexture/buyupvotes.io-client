848ec75294aa149c2f696001dd266b0684415ec1
Yash-Sheliya-Apexture <yash@apexture.in>
2024-12-26 15:23:11 +0530

    add new code

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures the app works properly in subdirectories
+  build: {
+    outDir: "dist", // Ensure Vite outputs the build here
+  },
   // Proxy configuration
   proxy: {
     '/api': {
