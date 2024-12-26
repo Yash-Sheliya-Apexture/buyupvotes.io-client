742677f996388538cc065667ab3432183a8ebaef
Yash-Sheliya-Apexture <yash@apexture.in>
2024-12-26 14:58:12 +0530

    add new code

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,5 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: './', // Ensures the app works properly in subdirectories
-//   // Proxy configuration
-//   proxy: {
-//     '/api': {
-//       target: process.env.NODE_ENV === 'production' 
-//         ? 'https://buyupvotes-io-server.onrender.com'  // Production backend URL (Render)
-//         : 'http://localhost:5000', // Local development backend URL
-//       changeOrigin: true,
-//       rewrite: (path) => path.replace(/^\/api/, ''), // Rewrite path to remove '/api'
-//     },
-//   },
-// });
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// Determine the target URL based on the environment
-const targetUrl = process.env.VITE_API_URL || 'http://localhost:5000'; // Default to localhost if not set in env
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -31,7 +8,9 @@ export default defineConfig({
   // Proxy configuration
   proxy: {
     '/api': {
-      target: targetUrl, // Using the conditionally set target URL
+      target: process.env.NODE_ENV === 'production' 
+        ? 'https://buyupvotes-io-server.onrender.com'  // Production backend URL (Render)
+        : 'http://localhost:5000', // Local development backend URL
       changeOrigin: true,
       rewrite: (path) => path.replace(/^\/api/, ''), // Rewrite path to remove '/api'
     },
