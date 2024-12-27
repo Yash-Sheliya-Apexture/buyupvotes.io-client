7a3c4f026c5c3df98f7bfc51f30be5c0e7831d11
Yash-Sheliya-Apexture <yash@apexture.in>
2024-12-27 18:26:18 +0530

    add

--- src/assets/websiteImages/index.js
+++ src/assets/websiteImages/index.js
@@ -1,7 +1,11 @@
+// Import all images
+import light1 from "../Images/light1.png";
+import light2 from "../Images/light2.png";
+
 // Export images as an object
 const images = {
-  light1: new URL("../Images/light1.png", import.meta.url).href,
-  light2: new URL("../Images/light2.png", import.meta.url).href,
+  light1,
+  light2,
 };
 
 export default images;
